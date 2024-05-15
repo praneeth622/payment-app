5fe0be9b0e439b2cd856883744d9712af916c3e8
Praneeth <89689768+praneeth622@users.noreply.github.com>
2024-05-15 14:35:18 +0000

    Sign in page

--- frontend/src/pages/signin.js
+++ frontend/src/pages/signin.js
@@ -1,8 +1,63 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Signin = () => {
+  const [formData,setformData] = useState({
+    email:'',
+    password:''
+  })
+
+  function handleSubmit(){
+    console.log("Logined Sucessfully")
+    console.log(formData)
+  }
+  const handleChange=(e)=>{
+    setformData({ ...formData, [e.target.name]: e.target.value });
+  }
   return (
-    <div>Signin</div>
+    
+    <div className="flex justify-center items-center h-screen mx-3 ">
+      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-12 pt-6 pb-6 mb-4">
+        <div className="mb-4 font-bold text-black-700 text-2xl text-center"> SignUp Page </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-2 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="********"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="flex items-center justify-between">
+        <button
+            className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+            type="submit"
+        >
+            Sign In
+        </button>
+        </div>
+      </form>
+    </div>
   )
 }
 
--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -18,10 +18,10 @@ const Signup = () => {
     console.log('Form submitted:', formData);
   };
   return (
-  <div className="flex justify-center items-center h-screen mx-3">
+  <div className="flex justify-center items-center h-screen mx-3 ">
   <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-12 pt-6 pb-6 mb-4">
     <div className="mb-4 font-bold text-black-700 text-2xl text-center"> SignUp Page </div>
-    <div className="mb-6">
+    <div className="mb-6 w-80">
       <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="firstName">
         First Name
       </label>
@@ -36,12 +36,12 @@ const Signup = () => {
         required
       />
     </div>
-    <div className="mb-4">
+    <div className="mb-4 ">
       <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="lastName">
         Last Name
       </label>
       <input
-        className="shadow appearance-none border rounded w-full py-3 px-5 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        className="shadow appearance-none border rounded w-full py-2 px-5 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         id="lastName"
         type="text"
         placeholder="Last Name"
@@ -56,7 +56,7 @@ const Signup = () => {
         Email
       </label>
       <input
-        className="shadow appearance-none border rounded w-full py-3 px-5 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        className="shadow appearance-none border rounded w-full py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         id="email"
         type="email"
         placeholder="Email"
@@ -71,7 +71,7 @@ const Signup = () => {
         Password
       </label>
       <input
-        className="shadow appearance-none border rounded w-full py-3 px-5 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+        className="shadow appearance-none border rounded w-full py-2 px-2 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
         id="password"
         type="password"
         placeholder="********"
@@ -83,14 +83,15 @@ const Signup = () => {
     </div>
     <div className="flex items-center justify-between">
     <button
-        className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
         type="submit"
-      >
+    >
         Sign Up
     </button>
     </div>
   </form>
-</div>)
+  </div>
+)
 }
 
 export default Signup
